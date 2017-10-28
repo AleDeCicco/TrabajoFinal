4ef5dade0da70c8dbcead4d092c1da81e12b3da3
AleDeCicco <31454517+AleDeCicco@users.noreply.github.com>
2017-10-28 16:29:22 -0300

    Actualizada segun clase transporte

--- src/Bicicleta.php
+++ src/Bicicleta.php
@@ -1,21 +1,8 @@
-class Bicicleta implements Transporte
+class Bicicleta extends Transporte
 {
-      protected $nombre;
-      protected $tipo;
-  
       public function __construct($nombre, $tipo)
       {
             $this->nombre = $nombre;
             $this->tipo = $tipo;
-      }
-  
-      public function Nombre()
-      {
-            return $this->nombre;
-      }
- 
-      public function Tipo()
-      {
-            return $this->tipo;
       }
 }
