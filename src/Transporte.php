fd7ca2d1b7bba95f676e81e740d5e2e40b764f14
delfi-martin <31671160+delfi-martin@users.noreply.github.com>
2017-10-28 16:26:03 -0300

    Create Transporte.php

---
+++ src/Transporte.php
@@ -0,0 +1,25 @@
+<?php
+
+abstract class Transporte
+{
+	
+	protected $nombre = '';
+
+	protected $tipo = '';
+
+
+	public function Nombre()
+	{
+
+		return $this->nombre;
+
+	}
+
+	public function Tipo()
+	{
+
+		return $this->tipo;
+
+	}
+
+} 
