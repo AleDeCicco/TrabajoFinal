c4b2685f1f7b55ab4cd343b9a72412b843b044fe
delfi-martin <31671160+delfi-martin@users.noreply.github.com>
2017-10-28 11:30:24 -0300

    Sintax Issue Fixed

--- src/InterfazBoleto.php
+++ src/InterfazBoleto.php
@@ -1,5 +1,5 @@
 <?php
 
 interface Boleto {
-	 public function infoBoleto(numero_serie); //muestra información del boleto (Su fecha, El tipo de boleto (Normal, Plus, Medio), el saldo de la tarjeta. El numero de linea y el ID de la tarjeta.)
+	 public function infoBoleto($numero_serie); //muestra información del boleto (Su fecha, El tipo de boleto (Normal, Plus, Medio), el saldo de la tarjeta. El numero de linea y el ID de la tarjeta.)
 }
