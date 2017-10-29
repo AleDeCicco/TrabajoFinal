27426591680183fd75c3a6b0bf2df8b5dbfe0647
delfi-martin <31671160+delfi-martin@users.noreply.github.com>
2017-10-29 12:29:27 -0300

    Update InterfazViaje.php

--- src/InterfazViaje.php
+++ src/InterfazViaje.php
@@ -2,7 +2,7 @@
 
 namespace TrabajoFinal;
 
-interface Viaje {
+interface Inter_Viaje {
 	public function tipo(); //devuelve el tipo de viaje
 	public function monto(); //devuelve el monto de viaje
 	public function transporte(); //devuelve el transporte usado
