88b72f74cc1380990a4a51303cb6af06e17065b9
delfi-martin <31671160+delfi-martin@users.noreply.github.com>
2017-10-29 18:01:02 -0300

    Update Viaje.php

--- src/Viaje.php
+++ src/Viaje.php
@@ -4,36 +4,33 @@ namespace TrabajoFinal;
 
 class viaje implements Inter_Viaje 
 {
-      protected $tipo;
-      protected $monto;
-      protected $transporte;
-      protected $tiempo;
+    protected $tipo;
+    protected $monto;
+    protected $transporte;
+    protected $tiempo;
   
-      public function __construct($tipo, $monto, $transporte, $tiempo)
-      {
-            $this->tipo = $tipo;
-            $this->monto = $monto;
-            $this->transporte = $transporte;
-            $this->tiempo = $tiempo;
-      }
+    public function __construct($tipo, $monto, $transporte, $tiempo)
+    {
+          $this->tipo = $tipo;
+          $this->monto = $monto;
+          $this->transporte = $transporte;
+          $this->tiempo = $tiempo;
+    }
   
-      public function Tipo()
-      {
-            return $this->tipo;
-      }
-
-      public function Monto()
-      {
-            return $this->monto;
-      }
-
-      public function Transporte()
-      {
-            return $this->transporte;
-      }
-
-      public function Tiempo()
-      {
-            return $this->tiempo;
-      }
+    public function Tipo()
+    {
+        return $this->tipo;
+    }
+    public function Monto()
+    {
+        return $this->monto;
+    }
+    public function Transporte()
+    {
+        return $this->transporte;
+    }
+    public function Tiempo()
+    {
+        return $this->tiempo;
+    }
 }
