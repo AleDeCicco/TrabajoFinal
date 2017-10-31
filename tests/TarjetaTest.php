375bef2e6229183873e107fc6cb76da1862d8e94
delfi-martin <31671160+delfi-martin@users.noreply.github.com>
2017-10-30 21:14:46 -0300

    Testing Bug Fixed

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -9,31 +9,31 @@ class TarjetaTest extends TestCase {
 	public function testRecarga()
 	{
 
-		$tarjeta = new Tarjeta();
+		$tarjeta0 = new Tarjeta();
 		
 		for ( $i=0 ; $i<=624 ; $i++ )
 		{
 		
-			$tarjeta->Recargar( $i );
+			$tarjeta0->Recargar( $i );
 			
 			if ( $i == 332 )
 			{
 			
-				$this->assertEquals( 388 , $tarjeta->Saldo() );
+				$this->assertEquals( 388 , $tarjeta0->Saldo() );
 			
 			}elseif ( $i == 624 )
 			{
 
-				$this->assertEquals( 776 , $tarjeta->Saldo() );
+				$this->assertEquals( 776 , $tarjeta0->Saldo() );
 
 			}else
 			{
 
-				$this->assertEquals( $i , $tarjeta->Saldo() );
+				$this->assertEquals( $i , $tarjeta0->Saldo() );
 
 			}
 			
-			$tarjeta->Vaciar(); //Para que no acumule el saldo
+			$tarjeta0->Vaciar(); //Para que no acumule el saldo
 		
 		}
 
@@ -49,16 +49,16 @@ class TarjetaTest extends TestCase {
 
 		$monto = 200
 
-		$tarjeta = new Tarjeta();
-		$tarjeta->recargar( $monto );
+		$tarjeta1 = new Tarjeta();
+		$tarjeta1->recargar( $monto );
 
-		$tarjeta->pagar( $colectivo144Negro , '2016/06/30 22:50' , 'regular' );
+		$tarjeta1->pagar( $colectivo144Negro , '2016/06/30 22:50' , 'regular' );
 
-		$this->assertEquals( $monto - 9.7 , $tarjeta->Saldo() );
+		$this->assertEquals( $monto - 9.7 , $tarjeta1->Saldo() );
 
-		$tarjeta->pagar( $colectivo144Negro , '2016/06/30 22:45' , 'regular' );
+		$tarjeta1->pagar( $colectivo144Negro , '2016/06/30 22:45' , 'regular' );
 
-		$this->assertEquals( $monto - ( 9.7 * 2 ) , $tarjeta->Saldo() );
+		$this->assertEquals( $monto - ( 9.7 * 2 ) , $tarjeta1->Saldo() );
 
 	}
 	
