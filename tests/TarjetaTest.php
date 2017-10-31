cadf5c32abedfd7cd25928fc1a7561a36f0dc31e
delfi-martin <31671160+delfi-martin@users.noreply.github.com>
2017-10-30 21:06:17 -0300

    testPago Added

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -39,4 +39,27 @@ class TarjetaTest extends TestCase {
 
 	}
 	
+	public function testPago()
+	{
+
+		$colectivo144Negro = new Colectivo( '144 Negro' , 'Rosario Bus' );
+		$colectivo135 = new Colectivo( '135' , 'Rosario Bus' );
+		$bici = new Bicicleta( 1234 , 'Bicicleta' );
+		$bici = new Bicicleta( 5678 , 'Bicicleta' );
+
+		$monto = 200
+
+		$tarjeta = new Tarjeta();
+		$tarjeta->recargar( $monto );
+
+		$tarjeta->pagar( $colectivo144Negro , '2016/06/30 22:50' , 'regular' );
+
+		$this->assertEquals( $monto - 9.7 , $tarjeta->Saldo() );
+
+		$tarjeta->pagar( $colectivo144Negro , '2016/06/30 22:45' , 'regular' );
+
+		$this->assertEquals( $monto - ( 9.7 * 2 ) , $tarjeta->Saldo() );
+
+	}
+	
 }
