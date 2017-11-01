7c41eedbb1dcec4b6fefce353cb90a9019fcfd17
delfi-martin <31671160+delfi-martin@users.noreply.github.com>
2017-11-01 00:56:41 -0300

    Create Inter_Boleto.php

---
+++ src/Inter_Boleto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TrabajoFinal;
+
+interface Inter_Boleto
+{
+
+	public function __construct(Viaje $viaje, Tarjeta $tarjeta);
+	public function Viaje();
+	public function Tarjeta();
+	public function Imprimir();
+
+}
