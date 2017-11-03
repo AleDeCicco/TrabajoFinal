03dbb41db03be10a4dc3c8f98402902d42feaa71
delfi-martin <31671160+delfi-martin@users.noreply.github.com>
2017-11-03 16:25:27 -0300

    Create BoletoTest.php

---
+++ tests/BoletoTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace TrabajoFinal;
+
+use PHPUnit\Framework\TestCase;
+
+class TarjetaTest extends TestCase {
+
+	public function testBoleto(){
+
+		$tarjeta = new tarjeta(10);
+		$array = $tarjeta->viajesRealizados();
+		$boleto = new Boleto(end($array),$tarjeta);
+
+		$this->assertEquals(end($array)->Tipo(),$boleto->viaje->Tipo());
+
+		$this->assertEquals($tarjeta->Saldo(),$boleto->tarjeta->Saldo());
+
+		$this->assertEquals(end($array)->Tipo(),$boleto->Viaje()->Tipo());
+
+		$this->assertEquals($tarjeta->Saldo(),$boleto->Tarjeta()->Saldo());
+
+	}
+
+}
