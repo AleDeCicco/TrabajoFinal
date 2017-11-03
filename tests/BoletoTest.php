3100984c7b314c0b8e0e8ba40f4a7794130ecc97
delfi-martin <31671160+delfi-martin@users.noreply.github.com>
2017-11-03 17:18:04 -0300

    Update BoletoTest.php

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -10,8 +10,8 @@ class BoletoTest extends TestCase {
 
 		$tarjeta = new Tarjeta(10);
 		
-		$tipo = 'regular'
-		$monto = 9.7
+		$tipo = 'regular';
+		$monto = 9.7;
 		$colectivo = new Colectivo('Colectivo144Negro','Rosario Bus');
 		$tiempo = '2017/11/03 11:50';
 		
