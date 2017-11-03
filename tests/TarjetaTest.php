fe2a0b6b6affa081a339f46b9ca45881b13ef853
delfi-martin <31671160+delfi-martin@users.noreply.github.com>
2017-11-02 22:39:07 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -60,10 +60,10 @@ class TarjetaTest extends TestCase {
 
 		$this->assertEquals( $monto - 9.7 , $tarjeta1->Saldo() );
 
-		$this->assertEquals('regular' , end($ViajesRealizados)->Tipo());
-		$this->assertEquals(9.7 , end($ViajesRealizados)->Monto());
-		$this->assertEquals(Colectivo , end($ViajesRealizados)->Transporte());
-		$this->assertEquals('2017/07/24 10:50' , end($ViajesRealizados)->Tiempo());
+		$this->assertEquals('regular' , (end($ViajesRealizados))->Tipo());
+		$this->assertEquals(9.7 , (end($ViajesRealizados))->Monto());
+		$this->assertEquals(Colectivo , (end($ViajesRealizados))->Transporte());
+		$this->assertEquals('2017/07/24 10:50' , (end($ViajesRealizados))->Tiempo());
 
 		$tarjeta1->Pagar( $colectivo144Negro , '2017/07/24 11:45' , 'regular' );
 
