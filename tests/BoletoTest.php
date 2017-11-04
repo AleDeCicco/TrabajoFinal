fe7b5974885c030fd92a503f762ea84e8a301492
AleDeCicco <31454517+AleDeCicco@users.noreply.github.com>
2017-11-04 18:54:18 -0300

    Format

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -9,24 +9,22 @@ class BoletoTest extends TestCase {
 	public function testBoleto(){
 
 		$tarjeta = new Tarjeta(10);
-		
+
 		$tipo = 'regular';
 		$monto = 9.7;
 		$colectivo = new Colectivo('Colectivo144Negro','Rosario Bus');
 		$tiempo = '2017/11/03 11:50';
-		
+
 		$viaje = new Viaje ($tipo,$monto,$colectivo,$tiempo);
-		
 		$boleto = new Boleto($viaje,$tarjeta);
-		
+
 		$array = $tarjeta->viajesRealizados();
 
 		$this->assertEquals($tipo,$boleto->Viaje()->Tipo());
-
 		$this->assertEquals($tarjeta->Id(),$boleto->Tarjeta()->Id());
-		
+
 		$array = $boleto->Imprimir();
-		
+
 		$this->assertEquals($array[0],$boleto->Viaje()->Tiempo());
 		$this->assertEquals($array[1],$boleto->Viaje()->Monto());
 		$t = $array[2];
